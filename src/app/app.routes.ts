9aa82c2871d70609279a955d056356e43c901fc5
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-26 06:37:18 +0530

    feat(coffee-listing): add new component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CoffeeListingComponent } from './coffee-listing/coffee-listing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/coffee-listing', pathMatch: 'full'},
+  { path: 'coffee-listing', component: CoffeeListingComponent}
+];
---
+++ src/app/coffee-listing/coffee-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coffee-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './coffee-listing.component.html',
+  styleUrl: './coffee-listing.component.scss'
+})
+export class CoffeeListingComponent {
+
+}
