f7ff0934e49ac2ba945ac991d6fee63d25d8aebb
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-26 18:45:26 +0530

    feat(card): add new component

--- src/app/coffee-listing/coffee-listing.component.ts
+++ src/app/coffee-listing/coffee-listing.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+//component
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-coffee-listing',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './coffee-listing.component.html',
   styleUrl: './coffee-listing.component.scss'
 })
