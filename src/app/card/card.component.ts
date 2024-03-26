3631560d2584db5bfef0a84bbce4cdb8c61d46c6
Rohan Thomas Reji <rohanreji123@gmail.com>
2024-03-26 22:05:31 +0530

    feat(card): pass coffee listing through input

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//model
+import { CoffeeListing } from '../coffee-listing/coffee-listing.model';
+
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+@Input()
+coffeeListing: CoffeeListing[] = [];
 }
